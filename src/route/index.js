a5cb13ae3ee84ad68610ab32fab2d373bbd4d2dc
AlexNat1707 <saschagnatiuk@gmail.com>
2023-03-23 00:54:47 +0200

    @list

--- src/route/index.js
+++ src/route/index.js
@@ -1277,5 +1277,39 @@ router.get('/task31', function (req, res) {
 
 // ================================================================
 
+router.get('/list', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('list', {
+    layout: "basic",
+      list: [
+        { name: "First Button" },
+        { name: "Secondary Button" },
+        { name: "Error Button" },
+        { name: "Submit Button" },
+      ],
+
+      user: {
+        name: {
+          value: "Ivan",
+          text: "Ivanov",
+        },
+        role: {
+          text: "Admin",
+        },
+        isConfit: {
+          value: true,
+        },
+        age: {
+         value: 32,
+        }, 
+      },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
